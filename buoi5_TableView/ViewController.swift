2d03aa0519bde0066a509828516c335ecb3da601
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-18 16:13:51 -0600

    Initial Commit

---
+++ buoi5_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buoi5_TableView
+//
+//  Created by Kien Nguyen on 2021-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
